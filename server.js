45dda3762f147f6a713dea3baf18fa4f23bf5744
joey <joseph66magdaleno@yahoo.com>
2020-06-18 15:27:35 -0700

    changed file locations

---
+++ server.js
@@ -0,0 +1,28 @@
+// add required modules
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+
+// Create server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Read URL or JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//app.use("/api", apiRoutes);
+//app.use("/", htmlRoutes);
+
+// Include js files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// Use public folder
+app.use(express.static("public"));
+
+// Add listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
